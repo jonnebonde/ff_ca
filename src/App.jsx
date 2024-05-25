f826d6efa3715ac6e3875daccbe0b0aa49c8f9b0
jonnebonde <jonnebonde@gmail.com>
2024-05-25 23:17:01 +0200

    started setting up header

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import Header from "./Components/Layout/Header";
 
 export default function App() {
   return (
     <>
-    <Header />
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <button className="btn">Button</button>
-      <div className="p-4">
-      <h1 className="text-primary">Dette er en primær fargetekst</h1>
-      <p className="text-secondary">Dette er en sekundær fargetekst</p>
-      <button className="btn btn-accent">Klikk Meg</button>
-      <div className="border border-border p-2">Border Eksempel</div>
-      <div className="flex items-center">
-        <span className="text-stars">★</span>
-        <span className="text-stars-empty">★</span>
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<Header />} >
+        <Route index element={<h1>Home</h1>} />
+        <Route path="/:id" element={<h1>Specific product</h1>} />
+        <Route path="checkout" element={<h1>checkout</h1>} />
+        <Route path="checkoutSuccess" element={<h1>Checkout success</h1>} />
+        <Route path="contact" element={<h1>contact</h1>} />
+        <Route path="*" element={<h1>Not Found</h1>} />
+      </Route>
+      </Routes>
     </>
   );
 }
