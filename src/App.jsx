331cd2ef22f21d827230b0ae3ce9f22f0ce503a9
jonnebonde <jonnebonde@gmail.com>
2024-05-25 22:24:49 +0200

    installed tanStack query

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import "./App.css";
+import Header from "./Components/Layout/Header";
 
 export default function App() {
   return (
     <>
+    <Header />
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <button className="btn">Button</button>
+      <div className="p-4">
+      <h1 className="text-primary">Dette er en primær fargetekst</h1>
+      <p className="text-secondary">Dette er en sekundær fargetekst</p>
+      <button className="btn btn-accent">Klikk Meg</button>
+      <div className="border border-border p-2">Border Eksempel</div>
+      <div className="flex items-center">
+        <span className="text-stars">★</span>
+        <span className="text-stars-empty">★</span>
+      </div>
+    </div>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,4 +8,24 @@ export default {
   plugins: [
     daisyui
   ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#191622", // HSL(250, 21%, 11%) konvertert til HEX
+          "secondary": "#212529",
+          "accent": "#665c33", // HSL(44, 40%, 08%) konvertert til HEX
+          "neutral": "#d4b257", // HSL(44, 50%, 16%) konvertert til HEX
+          "base-100": "#ffffff",
+          "info": "#ffffff", // Ingen info, brukt hvit
+          "success": "#339b76", // HSL(152, 38%, 42%) konvertert til HEX
+          "warning": "#d4b257", // Samme som button-hover for konsistens
+          "error": "#ff0000",
+          "border": "#000000",
+          "stars": "#ffff00",
+          "stars-empty": "#808080",
+        },
+      },
+    ],
+  },
 };
